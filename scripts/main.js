e4a0ecbaeacc485ab8c526733ca1ef912aa1f26d
Obob <obobob1to8@gmail.com>
2024-04-07 13:18:42 +0200

    safety commit
    implemented a couple of features one of which is the dark mode even though there is still room for improvement

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,32 @@
-const setDarkmode = () => {
+// Show scrollbar on scroll
+// document.addEventListener('scroll', function () {
+//     this.style.scrollbarWidth = 'auto';
+// });
 
+
+const themeToggle = document.querySelector('.themes > button');
+const currentTheme = document.querySelector('.themes > p');
+let darkMode = true;
+
+const setDarkmode = () => {
+    darkMode = !darkMode;
+    if (darkMode) {
+        document.documentElement.style.setProperty('--color-bg-base', '#000');
+        document.documentElement.style.setProperty('--color-txt-base', '#fff');
+        currentTheme.innerText = 'dark'
+        currentTheme.style.color = '#fff'
+        currentTheme.parentNode.style.background = '#ffffff20'
+    } else {
+        document.documentElement.style.setProperty('--color-bg-base', '#fff');
+        document.documentElement.style.setProperty('--color-txt-base', '#000');
+        currentTheme.innerText = 'light'
+        currentTheme.style.color = '#000'
+        currentTheme.parentNode.style.background = '#00000020'
+    }
 
 }
+
+themeToggle.addEventListener('click', setDarkmode);
 
 window.addEventListener('DOMContentLoaded', () => {
     initApp()
